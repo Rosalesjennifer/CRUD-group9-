ad78017d4a3431949317d05f24792a95cfffbb01
Rosalesjennifer <gonzales23jennifer@gmail.com>
2024-12-05 19:31:48 +0800

    commit

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -3,13 +3,13 @@ import { getFirestore, initializeFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyC0JvdreUdm0HMdyManfIrx_Z-Y0A7-Ioo",
-	authDomain: "crud-final-a4a4d.firebaseapp.com",
-	projectId: "crud-final-a4a4d",
-	storageBucket: "crud-final-a4a4d.firebasestorage.app",
-	messagingSenderId: "774960951604",
-	appId: "1:774960951604:web:6d030d727ab0ee3945f561",
-	measurementId: "G-996B2YS7MZ"
+	apiKey: "AIzaSyD8cEgBS3mV4_1Lmfwb6JLaPVmT-ZhN1Qc",
+	authDomain: "taskmanagercrud-6694e.firebaseapp.com",
+	projectId: "taskmanagercrud-6694e",
+	storageBucket: "taskmanagercrud-6694e.firebasestorage.app",
+	messagingSenderId: "486920153539",
+	appId: "1:486920153539:web:655448dd98d3eac7af3576",
+	measurementId: "G-E88WB1V18J"
   };
 
 // Initialize Firebase
